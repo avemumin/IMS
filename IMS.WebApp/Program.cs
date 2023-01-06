39ef6741df87bf30098f06349644cb8680ac2802
Jacek Szczepański <jacogitproject@gmail.com>
2023-01-06 17:41:43 +0100

    Add simple page and clear trash data

--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -2,17 +2,19 @@ using IMS.Plugins.InMemory;
 using IMS.UseCases.Inventories;
 using IMS.UseCases.Inventories.Interfaces;
 using IMS.UseCases.PluginInterfaces;
-using IMS.WebApp.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
 
 builder.Services.AddSingleton<IInventoryRepository, InventoryRepository>();
 builder.Services.AddTransient<IViewInventoriesByNameUseCases, ViewInventoriesByNameUseCases>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

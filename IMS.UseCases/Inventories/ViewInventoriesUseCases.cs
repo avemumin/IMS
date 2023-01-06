e22d1a05cbacefe420dae28392a1d755a8451704
Jacek Szczepański <jacogitproject@gmail.com>
2023-01-06 16:45:37 +0100

    Clean architecture project part

---
+++ IMS.UseCases/Inventories/ViewInventoriesUseCases.cs
@@ -0,0 +1,20 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases.Inventories
+{
+    public class ViewInventoriesUseCases
+    {
+        private readonly IInventoryRepository _inventoryRepository;
+
+        public ViewInventoriesUseCases(IInventoryRepository inventoryRepository)
+        {
+            _inventoryRepository = inventoryRepository;
+        }
+
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
+        {
+            return await _inventoryRepository.GetInventoryByNameAsync(name);
+        }
+    }
+}

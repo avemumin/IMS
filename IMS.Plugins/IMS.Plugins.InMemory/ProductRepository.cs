a998fe4dd617570a9766bc2744e6604f8a994a84
Jacek Szczepański <jacogitproject@gmail.com>
2023-01-07 20:15:17 +0100

    Product list components and product list item

---
+++ IMS.Plugins/IMS.Plugins.InMemory/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.Plugins.InMemory
+{
+    public class ProductRepository : IProductRepository
+    {
+        private List<Product> _listOfProducts;
+
+        public ProductRepository()
+        {
+            _listOfProducts = new List<Product>()
+            {
+                new Product() {ProductId = 1, ProductName = "Bike", Price = 150, Quantity = 10},
+                new Product() {ProductId = 2, ProductName = "Car", Price = 25000, Quantity = 5},
+                new Product() {ProductId = 3, ProductName = "Cat", Price = 3.41M, Quantity = 22},
+                new Product() {ProductId = 4, ProductName = "Dog", Price = 31, Quantity = 3},
+                new Product() {ProductId = 5, ProductName = "Nic", Price = 15, Quantity = 31}
+
+            };
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByNameAsync(string productName)
+        {
+            if (string.IsNullOrWhiteSpace(productName)) return await Task.FromResult(_listOfProducts);
+
+            return _listOfProducts.Where(x =>
+                x.ProductName.Contains(productName, StringComparison.CurrentCultureIgnoreCase));
+        }
+    }
+}
--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -2,6 +2,8 @@ using IMS.Plugins.InMemory;
 using IMS.UseCases.Inventories;
 using IMS.UseCases.Inventories.Interfaces;
 using IMS.UseCases.PluginInterfaces;
+using IMS.UseCases.Products;
+using IMS.UseCases.Products.Interfaces;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +18,10 @@ builder.Services.AddTransient<IViewInventoriesByNameUseCases, ViewInventoriesByN
 builder.Services.AddTransient<IAddInventoryUseCase, AddInventoryUseCase>();
 builder.Services.AddTransient<IEditInventoryUseCase, EditInventoryUseCase>();
 builder.Services.AddTransient<IViewInventoryByIdUseCase, ViewInventoryByIdUseCase>();
+
+
+builder.Services.AddSingleton<IProductRepository, ProductRepository>();
+builder.Services.AddTransient<IViewProductsByNameUseCase, ViewProductsByNameUseCase>();
 
 var app = builder.Build();
 

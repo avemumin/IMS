bca8479a6599e0a004ec6a9cf554fd7933ce1b2e
Jacek Szczepański <jacogitproject@gmail.com>
2023-01-21 21:07:50 +0100

    @bind-value :event revcall fix

--- IMS.UseCases/Activities/Interfaces/IPurchaseInventoryUseCase.cs
+++ IMS.UseCases/Activities/Interfaces/IPurchaseInventoryUseCase.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IMS.UseCases.Activities.Interfaces
+﻿namespace IMS.UseCases.Activities.Interfaces
 {
     public interface IPurchaseInventoryUseCase
     {
